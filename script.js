7bf83ffa3806a30fe55d5a6d585f7163e71eba07
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-25 19:42:20 -0300

    Requisito 09

--- script.js
+++ script.js
@@ -1,55 +1,62 @@
-let pixelBaseAltura = 5;
-let corPincel = document.getElementById('pincel');
-let corPaleta = document.getElementsByClassName('color');
-corPaleta[0].className += " selected";
+const pixelBaseAltura = 5;
+const corPincel = document.getElementById('pincel');
+const corPaleta = document.getElementsByClassName('color');
+corPaleta[0].className += ' selected';
 corPincel.className = 'black';
 
-window.onload = function () {
-  let quadroPixel = document.getElementById('pixel-board');
-
-  for (let index = 0; index < corPaleta.length; index += 1) {
-    corPaleta[index].addEventListener('click', selecionaCor); 
-  }
-
-  for (let index = 1; index <= pixelBaseAltura ** 2; index += 1) {
-    let tagPixel = criaTag('div','','pixel white','','');
-    tagPixel.addEventListener("click",pintaPixel);
-
-    quadroPixel.appendChild(tagPixel);
-  }
-};
-
 function selecionaCor(evento) {
-  let corSelecionada = evento.target;
+  const corSelecionada = evento.target;
 
   for (let index = 0; index < corPaleta.length; index += 1) {
-    corPaleta[index].classList.remove('selected'); 
+    corPaleta[index].classList.remove('selected');
   }
   corPincel.className = corSelecionada.className.replace('color ', '');
   evento.target.classList.add('selected');
 }
 
 function pintaPixel(evento) {
-  let cor = document.querySelector('.selected');
+  const cor = document.querySelector('.selected');
 
-  evento.target.className = "pixel" + cor.className.replace('color', '').replace(' selected', '');
+  evento.target.className = `pixel${cor.className.replace('color', '').replace(' selected', '')}`;
   console.log(cor.className);
 }
 
-function criaTag(tipo, id, classe, atributo, valor) {
-  let tag = document.createElement(tipo);
+function limpaTodosPixels() {
+  const todosPixels = document.getElementsByClassName('pixel');
 
-  if (id != '') {
+  for (let index = 0; index <= todosPixels.length; index += 1) {
+    todosPixels[index].className = 'pixel white';
+  }
+}
+
+function criaTag(tipo, id, classe) {
+  const tag = document.createElement(tipo);
+
+  if (id !== '') {
     tag.id = id;
   }
 
-  if (classe != '') {
+  if (classe !== '') {
     tag.className = classe;
-  }
-
-  if (atributo != '') {
-    tag[atributo] = valor;
   }
 
   return tag;
 }
+
+window.onload = function () {
+  const quadroPixel = document.getElementById('pixel-board');
+  const btnLimpar = document.getElementById('clear-board');
+  btnLimpar.innerText = 'Limpar';
+  btnLimpar.addEventListener('click', limpaTodosPixels);
+
+  for (let index = 0; index < corPaleta.length; index += 1) {
+    corPaleta[index].addEventListener('click', selecionaCor);
+  }
+
+  for (let index = 1; index <= pixelBaseAltura ** 2; index += 1) {
+    const tagPixel = criaTag('div', '', 'pixel white');
+    tagPixel.addEventListener('click', pintaPixel);
+
+    quadroPixel.appendChild(tagPixel);
+  }
+};
